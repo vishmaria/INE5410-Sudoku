2b1d528cd9bee604e558d9f26158f9c3353bde9f
vishmaria <maria.fernandabt99@gmail.com>
2023-06-24 15:48:26 -0300

    Adcionado tratamento de exceções e divisão Divisão de linhas/tabuleiro com abordagem parecida com T1. Tratamento de arquivo inexistente e argumentos inválidos.

--- helper.py
+++ helper.py
@@ -1,25 +1,26 @@
-import more_itertools
-from helper import *
-from multiprocessing import Process
+# import more_itertools
+# from multiprocessing import Process
 from threading import Thread
-import time
+
 
 # Funcao executada pelas Threads;
-def threadSolver(game, lineList, id):
+def threadSolver(game, lineList, id_thread):
     lines = list(lineList)
     for key in game:
         for item in lines:
-            if(len(set(game[key][item])) != 9):
-                errorDict[id].append(str(key)+str(item+1))
+            if (len(set(game[key][item])) != 9):
+                errorDict[id_thread].append(str(key) + str(item + 1))
     return 0
+
+
 # Processa a matriz e cria um dicionario com as informacoes necessarias para validacao
 def matrixProcessing(matrix):
-    puzzleLines = [matrix[i:i+9] for i in range(0, len(matrix), 9)] # Lista com o quebra cabeca dividido em linhas
+    puzzleLines = [matrix[i:i + 9] for i in range(0, len(matrix), 9)]  # Lista com o quebra cabeca dividido em linhas
 
-    aux = list(zip(*puzzleLines)) # gera uma lista de tuplas com a matriz transposta de puzzleLines
-    puzzleColumns = [list(item) for item in aux] # Lista com o quebra cabeca dividido em colunas
+    aux = list(zip(*puzzleLines))  # gera uma lista de tuplas com a matriz transposta de puzzleLines
+    puzzleColumns = [list(item) for item in aux]  # Lista com o quebra cabeca dividido em colunas
 
-    puzzleRegions = [] # Lista com o quebra cabeca dividido em regioes
+    puzzleRegions = []  # Lista com o quebra cabeca dividido em regioes
     # Parte feita com auxilio do chatGPT--- MUDAR!!!
     for i in range(3):
         for j in range(3):
@@ -29,30 +30,44 @@ def matrixProcessing(matrix):
 
     # Cria dicionario com as tres matrizes
     listaGame = [puzzleLines, puzzleColumns, puzzleRegions]
-    listaI = ['L', 'C', 'R'] # Lista dos indices
+    listaI = ['L', 'C', 'R']  # Lista dos indices
     gameDict = dict(zip(listaI, listaGame))
     return gameDict
+
 
 # Funcao geral executada pelos processos;
 def checkResult(game, gameI, pid, n_threads):
     for key in gameI:
 
         # Print do processo
-        print('Processo %i: resolvendo quebra-cabeças %i' %(pid, key))
+        print('Processo %i: resolvendo quebra-cabeças %i' % (pid, key))
         gameDict = matrixProcessing(game[key])
 
         # Divisao das linhas entre threads
-        listaI = [*range(9)] # Lista dos indices
-        threadLines = more_itertools.distribute(n_threads, listaI)
+        listaI = [*range(9)]  # Lista dos indices
+        # threadLines = more_itertools.distribute(n_threads, listaI)
+        threadLines = list()
+        chunk_lines = len(listaI) // n_threads
+        r = len(listaI) % n_threads
 
-        #Criacao das threads e diconario global de erros
+        begin = 0
+
+        for i in range(n_threads):
+            end = begin + chunk_lines
+            if r > 0:
+                end += 1
+                r -= 1
+            threadLines[i] = listaI[begin:end]
+            begin = end
+
+        # Criacao das threads e diconario global de erros
         threadList = []
         global errorDict
-        errorDict = {k+1: [] for k in range(n_threads)}
+        errorDict = {k + 1: [] for k in range(n_threads)}
 
         # Inicializacao das threads
         for i in range(n_threads):
-            threadList.append(Thread(target=threadSolver, args =(gameDict, threadLines[i], (i+1))))
+            threadList.append(Thread(target=threadSolver, args=(gameDict, threadLines[i], (i + 1))))
             threadList[i].start()
         for i in range(n_threads):
             threadList[i].join()
@@ -60,15 +75,14 @@ def checkResult(game, gameI, pid, n_threads):
         # Extrai informacoes necessarias para o print
         errorCount = 0
         errorList = []
-        for key in errorDict:
-            threadErrors = ', '.join(list(errorDict[key]))
-            errorCount += len(list(errorDict[key]))
-            errorStr = ('T%i: %s' %(key, threadErrors))
+        for key_error in errorDict:
+            threadErrors = ', '.join(list(errorDict[key_error]))
+            errorCount += len(list(errorDict[key_error]))
+            errorStr = ('T%i: %s' % (key_error, threadErrors))
             errorList.append(errorStr)
-        
+
         # Print dos erros
         if errorCount > 0:
-            print('Processo %i: %i erros encontrados (%s)' %(pid, errorCount, '; '.join(errorList) ))
+            print('Processo %i: %i erros encontrados (%s)' % (pid, errorCount, '; '.join(errorList)))
         else:
-            print('Processo %i: %i erros encontrados' %(pid, errorCount))
-
+            print('Processo %i: %i erros encontrados' % (pid, errorCount))
--- main.py
+++ main.py
@@ -1,44 +1,77 @@
 # Trabalho 2 INE-5410 
-import more_itertools
+from sys import argv
 from helper import *
 from multiprocessing import Process
 from threading import Thread
 import time
 
-start_time = time.time()
 
-n_process = 1
-n_threads = 2
-processList = []
+def inputs(argv):
+    file = argv[1]
+    if not file:
+        print("O arquivo não existe")
+        return
+    n_process = int(argv[2])
+    n_threads = int(argv[3])
+    if n_process < 1 or n_threads < 1:
+        print("Really?")
+        return
+    return file, n_process, n_threads
+def main():
 
-puzzleList = [] # Lista com os quebra cabecas
-# Abre o arquivo e divide os cenarios:
-with open("sudoku_boards.txt", "r") as f:
-    contents = f.read()
-    puzzleList = contents.split('\n\n')
+    file, n_process, n_threads = inputs(argv)
 
-# Remove quebra de linha dos cenarios:
-for i in range(len(puzzleList)):
-    item = puzzleList[i].replace('\n', '')
-    item = [*item]
-    puzzleList[i] = item
+    processList = []
 
-# Cria dicionario com cenarios:
-listaI = [*range(1, (len(puzzleList))+ 1)] # Lista dos indices
-puzzleDict = dict(zip(listaI, puzzleList))
+    start_time = time.time()
 
-# Divide os jogos entre os processos// Falta otimizar para que nao precise ser passado o diconario completo de jogos para cada thread
-gameIds = more_itertools.distribute(n_process, listaI)
+    puzzleList = list()  # Lista com os quebra cabecas
+    # Abre o arquivo e divide os cenarios:
+    with open(file, 'r') as f:
+        contents = f.readlines()
+        puzzleList = contents.split('\n\n')
 
-# Criacao dos processos
-if __name__ == '__main__': # caso for o programa principal
-    for i in range(n_process):
-        processName = i+1
-        process = Process(target=checkResult, args=(puzzleDict, gameIds[i], processName, n_threads), name = str(i+1))
-        processList.append(process)
-        process.start()
+    # Remove quebra de linha dos cenarios:
+    for i in range(len(puzzleList)):
+        puzzleList = [i for i in puzzleList if i != '\n']
 
-    for process in processList:
-        process.join()
+    # Cria dicionario com cenarios:
+    listaI = [*range(1, (len(puzzleList)) + 1)]  # Lista dos indices
+    puzzleDict = dict(zip(listaI, puzzleList))
 
-    print("--- %s seconds ---" % (time.time() - start_time))
+    # Divide os jogos entre os processos.
+    # Abordagem parecida com o T1, tratando casos de mais processos que jogos e vice-versa
+    # fazendo a divisão inteira de células no tabuleiro.
+    # TODO: Falta otimizar para que nao precise ser passado o dicionario completo de jogos para cada thread
+    if n_process > len(puzzleDict):
+        n_process = len(puzzleDict)
+    if n_threads > len(puzzleDict):
+        n_threads = len(puzzleDict)
+
+    r = len(puzzleDict) % n_process
+    n_puzzles = len(puzzleDict) // n_process
+
+    # TODO: verificar função chuncked do more_itertools.
+    gameIds = list()
+
+    # Criacao dos processos
+    begin = 0
+    if __name__ == '__main__':  # caso for o programa principal
+        for i in range(n_process):
+            processName = i + 1
+            end = begin + n_puzzles
+            if r > 0:
+                end += 1
+                r -= 1
+            process = Process(target=checkResult, args=(puzzleDict, gameIds[i], processName, n_threads),
+                              name=str(i + 1))
+            processList.append(process)
+            begin = end
+
+        for process in processList:
+            process.start()
+
+        for process in processList:
+            process.join()
+
+        print("--- %s seconds ---" % (time.time() - start_time))
